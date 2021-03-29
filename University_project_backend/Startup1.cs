a20d8e7ea2c95be41f03c609d47cdbfc88c66a43
Preetham Holenarasipura Deveraju <preetham.nov16@gmail.com>
2021-03-29 14:11:13 +0200

    New commit of authservice

---
+++ University_project_backend/Startup1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(University_project_backend.Startup1))]
+
+namespace University_project_backend
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            var provider = new Myauthenticationserverprovider();
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(15),
+                Provider=provider
+            };
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
